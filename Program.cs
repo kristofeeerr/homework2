b9f9bcb8f647fa874f92782afe9c1ddb7d7bb11e
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-02 01:02:57 +0300

    h
    h

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCoins;
+            string amountDiscount;
+            byte minimumDiscount;
+            uint maximumDiscount;
+            char currency = '$';
+            bool realMoney = true;
+            double bunPrice = 20.99;
+            float bunWeight = 75.5f;
+            var nameShop = "Konditer";
+            object purchaseAmount;
+
+
+        }
+    }
+}
